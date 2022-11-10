00aece53d9f984d6da43777c01a13fb35d0d4084
Dmitriy Yegorov <yegorovd14@gmail.com>
2022-11-10 00:05:11 +0000

    Add some cards functionality

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
-const slides = document.querySelectorAll('.slide');
+// Use slidesPlagin function and lines 5-6 to add some activity to slides functionality
+function slidesPlagin(activeSlide) {
+  const slides = document.querySelectorAll('.slide');
 
+  const randomSlide = Math.floor(Math.random() * 4 + 1);
+  slides[activeSlide ? activeSlide : randomSlide].classList.add('active');
 
-for(const slide of slides) {
+  for(const slide of slides) {
+    
+    slide.addEventListener('click', () => {
+      clearActiveAndPointerClasses();
+      slide.classList.add('active');
+      slide.classList.remove('point');
+    })
+  }
   
-  slide.addEventListener('click', () => {
-    clearActiveAndPointerClasses();
-    slide.classList.add('active');
-    slide.classList.remove('point');
-  })
+  const clearActiveAndPointerClasses = () => {
+    slides.forEach(slide => slide.classList.remove('active'));
+    slides.forEach(slide => slide.classList.add('point'));
+  }
 }
 
-const clearActiveAndPointerClasses = () => {
-  slides.forEach(slide => slide.classList.remove('active'));
-  slides.forEach(slide => slide.classList.add('point'));
-}
+slidesPlagin()
